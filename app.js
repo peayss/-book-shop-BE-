bf3472055c2a5a1b19c85798ed6b3bf39f4a7383
peayss <pys020831@naver.com>
2024-10-02 17:27:06 +0900

    app commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// dotenv 모듈
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+
+
+app.listen(process.env.PORT);
+
+const usersRouter = require('./routes/users');
+const booksRouter = require('./routes/books');
+const ordersRouter = require('./routes/orders');
+const cartsRouter = require('./routes/carts');
+const likesRouter = require('./routes/likes');
+
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
+app.use("/orders", ordersRouter);
+app.use("/likes", likesRouter);
+app.use("/carts", cartsRouter);
+
+
